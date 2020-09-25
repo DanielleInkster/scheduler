30485f38a5f0818ecd39e77ae617656e7abdf5ef
DanielleInkster <danielle.inkster@gmail.com>
2020-09-25 17:35:14 -0600

    Can add to db via PostMan

--- server/index.js
+++ server/index.js
@@ -3,18 +3,45 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const db = require('./db');
+const Appointment = require('./models/Appointments');
 
 const app = express();
 const apiPort = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(bodyParser.json());
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', (req, res) => {
-  res.send('Server Up!');
+  Appointment.find((err, appt) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(appt);
+    }
+  });
+});
+
+app.post('/create', (req, res) => {
+  const appt = new Appointment({
+    date: req.body.date,
+    time: req.body.time,
+    name: req.body.name,
+    project_name: req.body.project_name,
+    project_description: req.body.project_description,
+    email: req.body.email,
+  });
+
+  appt
+    .save()
+    .then((appt) => {
+      res.json(appt);
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
 });
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
