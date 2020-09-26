562404568a7be952bc9f441617c9bf4f593483f7
DanielleInkster <danielle.inkster@gmail.com>
2020-09-25 21:23:23 -0600

    fix for travis

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import DayPicker from '../src/containers/DayPicker.js';
 import './App.css';
 
@@ -7,10 +7,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <Router>
       <Switch>
         <Route exact match path="/" render={() => { return <DayPicker />; }} />
         <Route path="*" render={() => { return( <div>Page Not Found</div>)}} />
       </Switch>
+      </Router>
     </div>
   );
 }
