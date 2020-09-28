b75af81a84a4de9bea10127f1cd0f4125d84a7c4
DanielleInkster <danielle.inkster@gmail.com>
2020-09-27 18:53:42 -0600

    redirects to view saved request

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import { ModalContainer, ModalRoute } from 'react-router-modal';
 import DayPicker from '../src/containers/DayPicker.js';
 import Popup from '../src/components/Popup.js';
+import Request from '../src/components/Request.js';
 import './App.css';
 
 /* eslint-disable */
@@ -14,7 +15,7 @@ function App() {
         <Route exact match path="/" render={() => { return <DayPicker />; }} />
         <Route exact match path="/:date" render={() => { return <DayPicker />; }} />
         <ModalRoute component={Popup} path='/:date/CreateAppointment/:time' /> 
-        <ModalRoute component={Popup} path='/:date/ViewAppointment/:time' /> 
+        <Route exact match path='/:date/ViewAppointment/:time'render={() => { return <Request />; }} />
         <ModalRoute component={Popup} path='/:date/EditAppointment/:time' /> 
         <Route path="*" render={() => { return( <div>Page Not Found</div>)}} />
       </Switch>
