f451a27d04ffe2b66f19bcefac31a47f1a4db83c
DanielleInkster <danielle.inkster@gmail.com>
2020-09-27 18:41:47 -0600

    Modal links to form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { ModalContainer, ModalRoute } from 'react-router-modal';
 import DayPicker from '../src/containers/DayPicker.js';
+import Popup from '../src/components/Popup.js';
 import './App.css';
 
 /* eslint-disable */
@@ -11,8 +13,12 @@ function App() {
       <Switch>
         <Route exact match path="/" render={() => { return <DayPicker />; }} />
         <Route exact match path="/:date" render={() => { return <DayPicker />; }} />
+        <ModalRoute component={Popup} path='/:date/CreateAppointment/:time' /> 
+        <ModalRoute component={Popup} path='/:date/ViewAppointment/:time' /> 
+        <ModalRoute component={Popup} path='/:date/EditAppointment/:time' /> 
         <Route path="*" render={() => { return( <div>Page Not Found</div>)}} />
       </Switch>
+      <ModalContainer />
       </Router>
     </div>
   );
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Popup from 'reactjs-popup';
 import Form from './Form';
-import 'reactjs-popup/dist/index.css';
+import 'react-router-modal/css/react-router-modal.css';
 
-export default function FormPopup({ date, time, title }) {
+export default function FormPopup({ date, time }) {
   return (
-    <Popup trigger={<button>{title}</button>} position="right center">
-      <div>
-        <Form date={date} time={time} />
-      </div>
-    </Popup>
+    <div>
+      <Form date={date} time={time} />
+    </div>
   );
 }
