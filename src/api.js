7222ce0dd6b24172a35b2f72be22c8119c123933
DanielleInkster <danielle.inkster@gmail.com>
2020-09-27 21:36:23 -0600

    Can delete requests

--- src/api.js
+++ src/api.js
@@ -2,3 +2,12 @@ export const getAppts = (date) => fetch(`http://localhost:4000/${date}`).then((r
 
 export const getAppt = (date, time) =>
   fetch(`http://localhost:4000/${date}/${time}`).then((res) => res.json());
+
+export const deleteAppt = (date, time) =>
+  fetch(`http://localhost:4000/${date}/${time}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
