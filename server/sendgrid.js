48d63c154ed991292222c46ac4ec4b1afe89addb
DanielleInkster <danielle.inkster@gmail.com>
2020-09-28 20:33:28 -0600

    Can send emails on create

--- server/sendgrid.js
+++ server/sendgrid.js
@@ -3,7 +3,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
   to: 'danielle.inkster@gmail.com', // Change to your recipient
   from: 'MyFakeBusinessAcct@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
+  subject: 'Your Appointment Request with MyFakeBusiness',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
