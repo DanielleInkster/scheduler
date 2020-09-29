cee3606d644a1c7f3d8f445def12f1ed01854817
DanielleInkster <danielle.inkster@gmail.com>
2020-09-28 21:35:12 -0600

    Fix for appt loading

--- src/components/CreateAppt.js
+++ src/components/CreateAppt.js
@@ -10,6 +10,9 @@ export default function CreateAppt({ date, time }) {
 
   const onSubmit = async (data) => {
     await createAppt(data);
+    window.alert(
+      `Appointment created for ${data.date} at ${data.time}. A confirmation email has been sent to your inbox.`,
+    );
     history.push(`/${params.date}/ViewAppointment/${params.time}`);
   };
   return (
